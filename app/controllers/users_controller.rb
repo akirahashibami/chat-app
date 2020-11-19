71cd95522cebc5fe79f62148fcc8f847124a284a
akirahashibami <hashibami2207@gmail.com>
2020-11-19 14:33:49 +0000

    on the way edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,19 @@ class UsersController < ApplicationController
     @followers = current_user.followers
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to rooms_path
+    else
+      redirect_back(fallback_location: rooms_path)
+    end
+  end
+
   def follows
     user = User.find(params[:id])
     @users = user.followings
@@ -14,5 +27,11 @@ class UsersController < ApplicationController
   def followers
     user = User.find(params[:id])
     @users = user.followers
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :image
+
   has_many :messages
   has_many :entries
   has_many :rooms, through: :entries
