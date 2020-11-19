94f0879bffe367147fb92c357c4033cd03362f3c
akirahashibami <hashibami2207@gmail.com>
2020-11-19 09:15:55 +0000

    ゲストユーザー制限を更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
   root 'top#top'
-  post '/top/guest_sign_in', to: 'top#new_guest'
 
   devise_for :users, controllers:{
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  # ゲストログイン
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 
   resources :rooms, only: [:index, :show, :create] do
     resource :favorites, only: [:create, :destroy]
